7d5b94e0d19f8acbc7a73e5071c15ca887fe50d8
Nguyen Duc Nghia <nguyennghia1402aa@gmail.com>
2026-04-18 11:50:50 +0700

    them anh san pham

--- data/DATAimage.sql
+++ data/DATAimage.sql
@@ -7,55 +7,55 @@ INSERT INTO ProductImages (product_id, image_url) VALUES
 
 -- 2. Áo gym Adidas Pro
 INSERT INTO ProductImages (product_id, image_url) VALUES
-(2, N'https://link-ao-gym-adidas-pro-1.jpg'),
-(2, N'https://link-ao-gym-adidas-pro-2.jpg');
+(2, N'https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/816bb2cfb39d467a8871ac161dc56e80_9366/Designed_for_Training_Pro-Series_Tee_Black_JI8229_HM1.jpg'),
+(2, N'https://assets.adidas.com/images/h_2000,f_auto,q_auto,fl_lossy,c_fill,g_auto/ed16005d2da74951becc6ea34c3c93d6_9366/Designed_for_Training_Pro-Series_Tee_Black_JI8229_HM3_hover.jpg');
 
 -- 3. Quần short Puma Run
 INSERT INTO ProductImages (product_id, image_url) VALUES
-(3, N'https://link-quan-short-puma-run-1.jpg'),
+(3, N'https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/525791/01/fnd/VNM/fmt/png/Qu%E1%BA%A7n-short-d%E1%BB%87t-thoi-PUMA-RUN-nam'),
 (3, N'https://link-quan-short-puma-run-2.jpg');
 
 -- 4. Giày chạy Adidas Ultraboost
 INSERT INTO ProductImages (product_id, image_url) VALUES
-(4, N'https://link-giay-chay-adidas-ultraboost-1.jpg'),
+(4, N'https://bizweb.dktcdn.net/thumb/1024x1024/100/347/092/products/adidas-ultraboost-4-0-dna-gw2293-01.jpg'),
 (4, N'https://link-giay-chay-adidas-ultraboost-2.jpg');
 
 -- 5. Áo khoác Nike Windrunner
 INSERT INTO ProductImages (product_id, image_url) VALUES
-(5, N'https://link-ao-khoac-nike-windrunner-1.jpg'),
+(5, N'https://sneakerdaily.vn/wp-content/uploads/2024/07/Ao-Nike-Sportswear-Windrunner-Men-Hooded-Jacket-DA0002-018-1.jpg'),
 (5, N'https://link-ao-khoac-nike-windrunner-2.jpg');
 
 -- 6. Quần dài Reebok Training
 INSERT INTO ProductImages (product_id, image_url) VALUES
-(6, N'https://link-quan-dai-reebok-training-1.jpg'),
+(6, N'https://thesneakerhouse.com/wp-content/uploads/2020/09/REEBOK-TRAINING-ESSENTIALS-TRACK-PANTS-4.jpg'),
 (6, N'https://link-quan-dai-reebok-training-2.jpg');
 
 -- 7. Áo gym Under Armour HeatGear
 INSERT INTO ProductImages (product_id, image_url) VALUES
-(7, N'https://link-ao-gym-under-armour-heatgear-1.jpg'),
+(7, N'https://m.media-amazon.com/images/I/617C+Jd91RL._AC_SL1000_.jpg'),
 (7, N'https://link-ao-gym-under-armour-heatgear-2.jpg');
 
 -- 8. Giày chạy Puma Velocity
 INSERT INTO ProductImages (product_id, image_url) VALUES
-(8, N'https://link-giay-chay-puma-velocity-1.jpg'),
+(8, N'https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/311141/01/sv01/fnd/VNM/fmt/png/Gi%C3%A0y-Ch%E1%BA%A1y-B%E1%BB%99-Velocity-NITRO%E2%84%A2-4-N%E1%BB%AF'),
 (8, N'https://link-giay-chay-puma-velocity-2.jpg');
 
 -- 9. Áo bóng đá Adidas Real Madrid
 INSERT INTO ProductImages (product_id, image_url) VALUES
-(9, N'https://link-ao-bong-da-adidas-real-madrid-1.jpg'),
+(9, N'https://assets.adidas.com/images/w_600,f_auto,q_auto/78b62417f1e042aeb25e3353d278de3b_9366/Ao_DJau_San_Nha_Authentic_Real_Madrid_24-25_trang_IX8095_HM1.jpg'),
 (9, N'https://link-ao-bong-da-adidas-real-madrid-2.jpg');
 
 -- 10. Quần short Nike Flex
 INSERT INTO ProductImages (product_id, image_url) VALUES
-(10, N'https://link-quan-short-nike-flex-1.jpg'),
+(10, N'https://cdn.vuahanghieu.com/unsafe/0x900/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2022/02/qua-n-shorts-nike-pro-flex-vent-max-men-s-shorts-beige-cj1957-320-size-m-620f5ced7e1e8-18022022154637.jpg'),
 (10, N'https://link-quan-short-nike-flex-2.jpg');
 
 -- 11. Áo khoác Puma Sport
 INSERT INTO ProductImages (product_id, image_url) VALUES
-(11, N'https://link-ao-khoac-puma-sport-1.jpg'),
+(11, N'https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_2000,h_2000/global/526607/01/mod01/fnd/VNM/fmt/png/%C3%81o-kho%C3%A1c-nam-ch%E1%BA%A1y-b%E1%BB%99-d%E1%BB%87t-VELOCITY'),
 (11, N'https://link-ao-khoac-puma-sport-2.jpg');
 
 -- 12. Giày Reebok Nano
 INSERT INTO ProductImages (product_id, image_url) VALUES
-(12, N'https://link-giay-reebok-nano-1.jpg'),
+(12, N'https://www.jordan1.vn/wp-content/uploads/2023/09/3-96c1-86a2dfdfda99-reebok-reebok-nano-x2-shoes_19724927_44532787_1000_04878358491745cf84b6a29ff6561f13.png'),
 (12, N'https://link-giay-reebok-nano-2.jpg');
